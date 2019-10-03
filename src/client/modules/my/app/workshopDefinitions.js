6d73223ee78e7c3f3c0e9781617c1a1a6fc7435a
mshanemc <shane.mclaughlin@salesforce.com>
2019-10-03 07:50:35 -0500

    placeholder images + mobile breakpoints

--- src/client/modules/my/app/workshopDefinitions.js
+++ src/client/modules/my/app/workshopDefinitions.js
@@ -20,9 +20,10 @@ const workshops = [
         creator: 'Mike White',
         description:
             'Close your eyes and imagine you are the equipment manager at a major manufacturing warehouse. You are making your inspection rounds and realize how manual the process is. You think to yourself, wouldn’t it be awesome if I had an app that allowed me to track and monitor all the machines at once and have insights from the equipment data automatically sent to me. Now open your eyes, and watch how Mike was able to make that a reality in less than 30 minutes on the Lightning Platform.',
-        videoURL: 'https://www.youtube.com/embed/gyGt386eAqw?start=3532',
+        // videoURL: 'https://www.youtube.com/embed/gyGt386eAqw?start=3532',
         launchURL: launcher + 'https://github.com/MBWhite01/EquipmentCalibration',
-        docsURL: 'https://github.com/MBWhite01/EquipmentCalibration'
+        docsURL: 'https://github.com/MBWhite01/EquipmentCalibration',
+        imgURL: '/resources/images/coming_soon_equipment.png'
         // docsURL: 'https://org62.my.salesforce.com/sfc/p/000000000062/a/0M000000J8MN/H9L7tSJBduvnvz4KIgtTjsyF6s98WsCw7nbbi.e03sA'
     },
     {
@@ -33,8 +34,9 @@ const workshops = [
             'Meet Michelle. She loves connecting with people. In this day and age she understands that the easiest way to connect with people is through text message. Odds are you’re probably reading this on your phone as we speak. So as someone who wants to stay connected with her customers and colleagues, Michelle needed a way to do so. Not just when she’s on the go but also when her customers are on the go. See how she built an app to SMS text message members in a salesforce campaign updates, changes and more in less than 30 minutes built on the Lightning Platform.',
         docsURL: 'https://github.com/mshanemc/twilio-text-app',
         // docsURL: 'https://org62.my.salesforce.com/sfc/p/000000000062/a/0M000000J8MS/PaJMIhyojXOsYxgLgkeH0DGokBdzha3fa7dtdgVVFO0',
-        videoURL: 'https://www.youtube.com/embed/gyGt386eAqw?start=3532',
-        launchURL: launcher + 'https://github.com/mshanemc/twilio-text-app'
+        // videoURL: 'https://www.youtube.com/embed/gyGt386eAqw?start=3532',
+        launchURL: launcher + 'https://github.com/mshanemc/twilio-text-app',
+        imgURL: '/resources/images/coming_soon_sms.png'
     },
 
     {
@@ -45,8 +47,9 @@ const workshops = [
             'Donald loves a fast car, but not just any kind of fast car, he likes race cars. Daytona, Indy, milk at the finish line. That kind of car. And like most car aficionados he dreams to one day manage a race team of his own. And nothing is more iconic for a race car than their sponsorship logos. See how Donald was able to create a sponsorship tracking app for his team #TheAudiArmy in less than 30 minutes built on the Lightning Platform.',
         docsURL: 'https://github.com/mshanemc/audiarmy',
         // docsURL: 'https://org62.my.salesforce.com/sfc/p/000000000062/a/0M000000J8MX/1F71i_RoP_ahlK7CpjV3wGjQy23q6RyMSRfkC2e5uWw',
-        videoURL: 'https://www.youtube.com/embed/gyGt386eAqw?start=3532',
-        launchURL: launcher + 'https://github.com/mshanemc/audiarmy'
+        // videoURL: 'https://www.youtube.com/embed/gyGt386eAqw?start=3532',
+        launchURL: launcher + 'https://github.com/mshanemc/audiarmy',
+        imgURL: '/resources/images/coming_soon_audi.png'
     }
 ];
 
